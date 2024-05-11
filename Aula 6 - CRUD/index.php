fbe6f7dcfe5c2a27a261b7cb2a85eb7bf7ac8ca7
Diego Oliveira Ramos <oliver.diegoramos@gmail.com>
2024-05-11 10:00:21 -0300

    Aula 7 - Quiz

--- Aula 6 - CRUD/index.php
+++ Aula 6 - CRUD/index.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FORMULARIO</title>
 </head>
+
 <body>
     <main>
         <h1>
@@ -30,4 +32,5 @@
         </form>
     </main>
 </body>
+
 </html>
---
+++ Aula 7 - Mother's day/script.js
@@ -0,0 +1,59 @@
+let current_question_index = 0
+
+document.addEventListener('DOMContentLoaded', get_questions)
+document.addEventListener('click', clicado)
+
+function get_questions() {
+    const QUESTION_CARD = document.getElementById('question-card')
+    const QUESTION_ELEMENT = document.querySelector('question')
+    const OPTIONS = document.querySelectorAll('input[type=radio]')
+    const OPTIONS_LABELS = document.querySelectorAll('label')
+    const BUTTON = document.getElementById('submit')
+    const RESULT_DIV = document.getElementById('result')
+    let questions = []
+
+    fetch('get_questions.php')
+        .then(response => response.json())
+        .then(data => {
+            questions = data
+            show_questions()
+        })
+
+}
+
+function show_questions()
+{
+    if (current_question_index < questions.length)
+    {
+        const QUESTION = questions[current_question_index]
+        QUESTION_ELEMENT.textContent = QUESTION.question
+        OPTIONS_LABELS[0].textContent = QUESTION.option1
+        OPTIONS_LABELS[1].textContent = QUESTION.option2
+        OPTIONS_LABELS[2].textContent = QUESTION.option3
+        OPTIONS_LABELS[3].textContent = QUESTION.option4
+    }
+    else
+    {
+        show_result()    
+    }
+}
+
+function show_result()
+{
+    RESULT_DIV.textContent = 'Fim do Quiz'
+}
+
+function clicado()
+{
+    const SELECT_OPTION = document.querySelector('input[name=option]:checked')
+    if (SELECT_OPTION)
+    {
+        const ANSWER = SELECT_OPTION.nextElementSibling.textContent
+        current_question_index++
+        show_questions()
+    }
+    else
+    {
+        alert('Selecione uma opção antes de enviar')
+    }
+}
